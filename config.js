2a1d706687de2db120689c926d57c0793c78d530
Nimasha2010 <nimashaadithya2010@gmail.com>
2026-07-18 09:15:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Nimasha2010/NIMIYA-MD/blob/main/images/WhatsApp%20Image%202026-07-15%20at%2014.27.07.jpeg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 NIMIYA-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
